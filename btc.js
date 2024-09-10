7f75afe5328ec949deba23710b67bd193c1f72e5
RobinP <poncon.robin@gmail.com>
2024-09-10 15:59:15 +0200

    adding api + catch error + show datas

--- btc.js
+++ btc.js
@@ -1,1 +1,22 @@
-console.log("test");
+const axios = require("axios");
+
+async function main() {
+	const currency = process.argv[2] ? process.argv[2].toUpperCase() : "USD";
+
+	try {
+		const url = "https://api.coindesk.com/v1/bpi/currentprice.json";
+		const { data } = await axios.get(url);
+
+		if (!data.bpi[currency]) {
+			throw new Error("Devise inconnue");
+		}
+
+		const rate = data.bpi[currency].rate;
+		const updatedAt = data.time.updated;
+		console.log(`> 1 BTC = ${rate} ${currency} (${updatedAt})`);
+	} catch (err) {
+		console.log(err.toString());
+	}
+}
+
+main();
